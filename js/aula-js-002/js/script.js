1a151ddf99b28a6c060e11258a70e7ef6ef3b52c
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-09 20:56:31 -0300

    funções e eventos

---
+++ js/aula-js-002/js/script.js
@@ -0,0 +1,7 @@
+function minhaFuncao() {
+	document.getElementById("demo").innerHTML = "Texto alterado";
+}
+
+function mudaTexto() {
+	document.getElementById("outro").innerHTML = "Mudei o nome no select";
+}
