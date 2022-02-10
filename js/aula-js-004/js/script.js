10d5d8cab848450858e1d028aab9c32b86ba5b20
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-09 23:34:27 -0300

    arrays e condicionais

---
+++ js/aula-js-004/js/script.js
@@ -0,0 +1,9 @@
+var carros = ["Fusca", "Gol", "Corsa"];
+document.getElementById("carros").innerHTML = carros[2];
+
+var nome = "Rafael";
+if (nome === "Rafael") {
+	document.getElementById("saudacao").innerHTML = "Seja Bem-vindo!";
+} else {
+	document.getElementById("saudacao").innerHTML = "Não é o Rafael!";
+}
